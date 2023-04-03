5a8d084468e812d927bd3628a2e7f7b45c972941
johnie261 <njorogejohn261@gmail.com>
2023-04-04 01:26:49 +0300

    Learn by following the video

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/cartContainer";
+import { useEffect } from "react";
+import { calculateTotal } from "./feature/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+  const {cartItems} = useSelector((store) => store.cart)
+  const {isOpen} = useSelector((store) => store.modal)
+  const dispatch = useDispatch();
 
+  useEffect(()=>{
+    dispatch(calculateTotal());
+  }, [cartItems])
+  return <main>
+    { isOpen && <Modal/> }
+    <Navbar />
+    <CartContainer />
+  </main>
+}
 export default App;
